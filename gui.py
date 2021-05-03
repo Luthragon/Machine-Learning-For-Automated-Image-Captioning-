bf2fab12a2bba669d09cb0320f79b82060712986
Farhan Eka <farhanekaf@gmail.com>
2021-05-03 20:46:59 +0700

    update button still problem

--- gui.py
+++ gui.py
@@ -36,7 +36,7 @@ def encode(image):
     return vec
 
 
-#asdasdasdasdas
+
 
 def greedy_search(pic):
     start = 'startseq'
@@ -57,51 +57,6 @@ def greedy_search(pic):
 
     return final
 
-'''
-def beam_search(image, beam_index = 3):
-    start = [wordtoix["startseq"]]
-    
-    # start_word[0][0] = index of the starting word
-    # start_word[0][1] = probability of the word predicted
-    start_word = [[start, 0.0]]
-    
-    while len(start_word[0][0]) < max_length:
-        temp = []
-        for s in start_word:
-            par_caps = pad_sequences([s[0]], maxlen=max_length)
-            e = image
-            preds = model.predict([e, np.array(par_caps)])
-            
-            # Getting the top <beam_index>(n) predictions
-            word_preds = np.argsort(preds[0])[-beam_index:]
-            
-            # creating a new list so as to put them via the model again
-            for w in word_preds:
-                next_cap, prob = s[0][:], s[1]
-                next_cap.append(w)
-                prob += preds[0][w]
-                temp.append([next_cap, prob])
-                    
-        start_word = temp
-        # Sorting according to the probabilities
-        start_word = sorted(start_word, reverse=False, key=lambda l: l[1])
-        # Getting the top words
-        start_word = start_word[-beam_index:]
-    
-    start_word = start_word[-1][0]
-    intermediate_caption = [ixtoword[i] for i in start_word]
-
-    final_caption = []
-
-    for i in intermediate_caption:
-        if i != 'endseq':
-            final_caption.append(i)
-        else:
-            break
-
-    final_caption = ' '.join(final_caption[1:])
-    return final_caption
-'''
 
 pickle_in = open("wordtoix.pkl", "rb")
 wordtoix = load(pickle_in)
@@ -112,12 +67,11 @@ model = load_model('model.h5')
 
 #initialise GUI
 top=tk.Tk()
-top.geometry('1280x600')
+top.geometry('1000x500')
 top.title('Caption Generator')
-top.configure(background='#CDCDCD')
-label2=Label(top,background='#CDCDCD', font=('arial',15))
-label1=Label(top,background='#CDCDCD', font=('arial',15))
-label=Label(top,background='#CDCDCD', font=('arial',15))
+top.configure(background='#fffbde')
+label3=Label(top, font=('arial',15))
+label=Label(top, font=('arial',15))
 sign_image = Label(top)
 def classify(file_path):
     global label_packed
@@ -126,23 +80,11 @@ def classify(file_path):
 
     pred = greedy_search(image)
     print(pred)
-    label.configure(foreground='#000', text= 'Greedy: ' + pred)
+    label.configure(foreground='#000', text=pred)
+    label3.configure(foreground='#000', text='Accuracy around 80%')
     label.pack(side=BOTTOM,expand=True)
+    label3.pack(side=BOTTOM,pady=50)
 
-    beam_3 = beam_search(image)
-    print(beam_3)
-    label1.configure(foreground='#011638', text = 'Beam_3: ' + beam_3)
-    label1.pack(side = BOTTOM, expand = True)
-    
-    beam_5 = beam_search(image, 5)
-    print(beam_5)
-    label2.configure(foreground='#228B22', text = 'Beam_5: ' + beam_5)
-    label2.pack(side = BOTTOM, expand = True)
-
-def show_classify_button(file_path):
-    classify_b=Button(top,text="Generate",command=lambda: classify(file_path),padx=10,pady=5)
-    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
-    classify_b.place(relx=0.79,rely=0.46)
 def upload_image():
     try:
         file_path=filedialog.askopenfilename()
@@ -152,19 +94,30 @@ def upload_image():
         sign_image.configure(image=im)
         sign_image.image=im
         label.configure(text='')
-        label1.configure(text='')
-        label2.configure(text='')
         show_classify_button(file_path)
     except:
         pass
-upload=Button(top,text="Upload an image",command=upload_image,padx=10,pady=5)
+
+def show_classify_button(file_path):
+    classify_b=Button(top,text="Proses",command=lambda: classify(file_path),padx=10,pady=10)
+    classify_b.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+    classify_b.pack(side=RIGHT, pady=50)
+
+
+exit = Button(text='Exit Aplikasi',command=exit)
+exit.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
+exit.pack(side=RIGHT, padx=50, pady=50)
+
+
+upload=Button(top,text="Pilih Gambar",command=upload_image,padx=10,pady=5)
 upload.configure(background='#364156', foreground='white',font=('arial',10,'bold'))
-upload.pack(side=BOTTOM,pady=50)
+upload.pack(side=RIGHT,padx=70)
 sign_image.pack(side=BOTTOM,expand=True)
 
 
 #label2.pack(side = BOTTOM, expand = True)
-heading = Label(top, text="Caption Generator",pady=20, font=('arial',22,'bold'))
-heading.configure(background='#CDCDCD',foreground='#FF6347')
-heading.pack()
+heading = Label(top, text="Pembuat Caption",pady=20, font=('arial',22,'bold'))
+
+heading.pack(side=TOP)
 top.mainloop()
+
